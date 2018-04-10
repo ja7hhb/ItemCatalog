13ea5a0afcd4deb4a65985085836feddd3e52dff
Kazuyuki Awaka <x.v.vb.ea@gmail.com>
2018-04-10 10:29:54 +0900

    change port number

--- catalogApp.py
+++ catalogApp.py
@@ -407,4 +407,4 @@ def deleteItem(catalog_id,item_id):
 if __name__ == '__main__':
     app.secret_key = 'super_secret_key'
     app.debug = True
-    app.run(host = '0.0.0.0', port = 4000)
+    app.run(host = '0.0.0.0', port = 5000)
